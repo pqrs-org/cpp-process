41536d368e362d9827185c4dbcc36e7eb9059162
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 10:07:02 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,125 +1,22 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/process.hpp>
 #include <pqrs/string.hpp>
 
-TEST_CASE("process") {
-  auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
-  auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-  {
-    std::string stdout;
-    std::string stderr;
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/bin/echo",
-                                 "hello",
-                             });
-    p.stdout_received.connect([&stdout](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stdout += c;
-      }
-    });
-    p.stderr_received.connect([&stderr](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stderr += c;
-      }
-    });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
+  "process"_test = [] {
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+    auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
 
-    p.wait();
-
-    REQUIRE(stdout == "hello\n");
-    REQUIRE(stderr == "");
-  }
-
-  {
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/usr/bin/yes",
-                             });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
-
-    p.kill(SIGHUP);
-    p.wait();
-  }
-
-  // Sleep and kill
-
-  {
-    std::string stdout;
-    std::string stderr;
-    auto p = std::make_unique<pqrs::process::process>(dispatcher,
-                                                      std::vector<std::string>{
-                                                          "/bin/sh",
-                                                          "-c",
-                                                          "echo hello; sleep 30; echo world",
-                                                      });
-    p->stdout_received.connect([&stdout](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stdout += c;
-      }
-    });
-    p->stderr_received.connect([&stderr](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stderr += c;
-      }
-    });
-    p->run();
-    std::cout << "pid: " << *(p->get_pid()) << std::endl;
-
-    // Wait until sleep is executed.
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-    auto start = std::chrono::system_clock::now();
-
-    p->kill(SIGKILL);
-    p = nullptr;
-
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-    REQUIRE(stdout == "hello\n");
-    REQUIRE(stderr == "");
-    REQUIRE(elapsed < 3000);
-  }
-
-  // Kill and wait automatically at destructor.
-
-  {
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/usr/bin/yes",
-                             });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
-  }
-
-  // Kill without wait (wait automatically at destructor)
-
-  {
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/usr/bin/yes",
-                             });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
-
-    p.kill(SIGHUP);
-  }
-
-  // SIGHUP will be ignored by program.
-
-  {
-    std::string stdout;
-    std::string stderr;
     {
+      std::string stdout;
+      std::string stderr;
       pqrs::process::process p(dispatcher,
                                std::vector<std::string>{
-                                   "./build/hello",
+                                   "/bin/echo",
+                                   "hello",
                                });
       p.stdout_received.connect([&stdout](auto&& buffer) {
         for (const auto& c : *buffer) {
@@ -131,96 +28,204 @@ TEST_CASE("process") {
           stderr += c;
         }
       });
-      p.run_failed.connect([] {
-        std::cout << "run_failed" << std::endl;
-      });
-      p.exited.connect([](auto&& status) {
-        std::cout << "exited: " << status << std::endl;
-      });
       p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
 
-      // Wait until `signal` in hello.cpp is called.
-      for (int i = 0; i < 10; ++i) {
-        std::cout << "." << std::flush;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-      }
-      std::cout << std::endl;
+      p.wait();
 
-      // SIGHUP is ignored.
+      expect(stdout == "hello\n");
+      expect(stderr == "");
+    }
+
+    {
+      pqrs::process::process p(dispatcher,
+                               std::vector<std::string>{
+                                   "/usr/bin/yes",
+                               });
+      p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
+
+      p.kill(SIGHUP);
+      p.wait();
+    }
+
+    // Sleep and kill
+
+    {
+      std::string stdout;
+      std::string stderr;
+      auto p = std::make_unique<pqrs::process::process>(dispatcher,
+                                                        std::vector<std::string>{
+                                                            "/bin/sh",
+                                                            "-c",
+                                                            "echo hello; sleep 30; echo world",
+                                                        });
+      p->stdout_received.connect([&stdout](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stdout += c;
+        }
+      });
+      p->stderr_received.connect([&stderr](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stderr += c;
+        }
+      });
+      p->run();
+      std::cout << "pid: " << *(p->get_pid()) << std::endl;
+
+      // Wait until sleep is executed.
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+      auto start = std::chrono::system_clock::now();
+
+      p->kill(SIGKILL);
+      p = nullptr;
+
+      auto end = std::chrono::system_clock::now();
+      auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+      expect(stdout == "hello\n");
+      expect(stderr == "");
+      expect(elapsed < 3000);
+    }
+
+    // Kill and wait automatically at destructor.
+
+    {
+      pqrs::process::process p(dispatcher,
+                               std::vector<std::string>{
+                                   "/usr/bin/yes",
+                               });
+      p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
+    }
+
+    // Kill without wait (wait automatically at destructor)
+
+    {
+      pqrs::process::process p(dispatcher,
+                               std::vector<std::string>{
+                                   "/usr/bin/yes",
+                               });
+      p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
+
       p.kill(SIGHUP);
     }
 
-    REQUIRE(stdout == "hello\n");
-    REQUIRE(stderr == "");
-  }
+    // SIGHUP will be ignored by program.
 
-  // stderr
+    {
+      std::string stdout;
+      std::string stderr;
+      {
+        pqrs::process::process p(dispatcher,
+                                 std::vector<std::string>{
+                                     "./build/hello",
+                                 });
+        p.stdout_received.connect([&stdout](auto&& buffer) {
+          for (const auto& c : *buffer) {
+            stdout += c;
+          }
+        });
+        p.stderr_received.connect([&stderr](auto&& buffer) {
+          for (const auto& c : *buffer) {
+            stderr += c;
+          }
+        });
+        p.run_failed.connect([] {
+          std::cout << "run_failed" << std::endl;
+        });
+        p.exited.connect([](auto&& status) {
+          std::cout << "exited: " << status << std::endl;
+        });
+        p.run();
 
-  {
-    std::string stdout;
-    std::string stderr;
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/bin/sh",
-                                 "-c",
-                                 "/not_found.sh"});
-    p.stdout_received.connect([&stdout](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stdout += c;
+        // Wait until `signal` in hello.cpp is called.
+        for (int i = 0; i < 10; ++i) {
+          std::cout << "." << std::flush;
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        std::cout << std::endl;
+
+        // SIGHUP is ignored.
+        p.kill(SIGHUP);
       }
-    });
-    p.stderr_received.connect([&stderr](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stderr += c;
-      }
-    });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
 
-    p.wait();
+      expect(stdout == "hello\n");
+      expect(stderr == "");
+    }
 
-    // Ensure stdout_received and stderr_received are called.
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // stderr
 
-    REQUIRE(stdout == "");
-    REQUIRE(stderr == "/bin/sh: /not_found.sh: No such file or directory\n");
-  }
+    {
+      std::string stdout;
+      std::string stderr;
+      pqrs::process::process p(dispatcher,
+                               std::vector<std::string>{
+                                   "/bin/sh",
+                                   "-c",
+                                   "/not_found.sh"});
+      p.stdout_received.connect([&stdout](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stdout += c;
+        }
+      });
+      p.stderr_received.connect([&stderr](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stderr += c;
+        }
+      });
+      p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
 
-  // Environment variable
+      p.wait();
 
-  {
-    std::string stdout;
-    std::string stderr;
-    pqrs::process::process p(dispatcher,
-                             std::vector<std::string>{
-                                 "/bin/sh",
-                                 "-c",
-                                 "echo $HOME"});
-    p.stdout_received.connect([&stdout](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stdout += c;
-      }
-    });
-    p.stderr_received.connect([&stderr](auto&& buffer) {
-      for (const auto& c : *buffer) {
-        stderr += c;
-      }
-    });
-    p.run();
-    std::cout << "pid: " << *(p.get_pid()) << std::endl;
+      // Ensure stdout_received and stderr_received are called.
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-    p.wait();
+      expect(stdout == "");
+      expect(stderr == "/bin/sh: /not_found.sh: No such file or directory\n");
+    }
 
-    // Ensure stdout_received and stderr_received are called.
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // Environment variable
 
-    pqrs::string::trim(stdout);
-    std::cout << "stdout: `" << stdout << "`" << std::endl;
+    {
+      std::string stdout;
+      std::string stderr;
+      pqrs::process::process p(dispatcher,
+                               std::vector<std::string>{
+                                   "/bin/sh",
+                                   "-c",
+                                   "echo $HOME"});
+      p.stdout_received.connect([&stdout](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stdout += c;
+        }
+      });
+      p.stderr_received.connect([&stderr](auto&& buffer) {
+        for (const auto& c : *buffer) {
+          stderr += c;
+        }
+      });
+      p.run();
+      std::cout << "pid: " << *(p.get_pid()) << std::endl;
 
-    REQUIRE(stdout != "");
-    REQUIRE(stderr == "");
-  }
+      p.wait();
 
-  dispatcher->terminate();
-  dispatcher = nullptr;
+      // Ensure stdout_received and stderr_received are called.
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      pqrs::string::trim(stdout);
+      std::cout << "stdout: `" << stdout << "`" << std::endl;
+
+      expect(stdout != "");
+      expect(stderr == "");
+    }
+
+    dispatcher->terminate();
+    dispatcher = nullptr;
+  };
+
+  return 0;
 }
